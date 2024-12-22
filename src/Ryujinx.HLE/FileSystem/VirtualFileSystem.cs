asfasagag/Ryujinx-Greem

apply patchanon's dev patches
https://boards.4chan.org/vp/thread/56789245#p56791847

src/Ryujinx.HLE/FileSystem/VirtualFileSystem.cs
@@ -27,9 +27,9 @@ namespace Ryujinx.HLE.FileSystem
 {
     public class VirtualFileSystem : IDisposable
     {
-        public static readonly string SafeNandPath = Path.Combine(AppDataManager.DefaultNandDir, "safe");
-        public static readonly string SystemNandPath = Path.Combine(AppDataManager.DefaultNandDir, "system");
-        public static readonly string UserNandPath = Path.Combine(AppDataManager.DefaultNandDir, "user");
+        public static readonly string SafeNandPath = Path.Combine(AppDataManager.DefaultNandDir, "safe_dev");
+        public static readonly string SystemNandPath = Path.Combine(AppDataManager.DefaultNandDir, "system_dev");
+        public static readonly string UserNandPath = Path.Combine(AppDataManager.DefaultNandDir, "user_dev");
 
         public KeySet KeySet { get; private set; }
         public EmulatedGameCard GameCard { get; private set; }
@@ -222,6 +222,7 @@ namespace Ryujinx.HLE.FileSystem
         public void ReloadKeySet()
         {
             KeySet ??= KeySet.CreateDefaultKeySet();
+            KeySet.SetMode(KeySet.Mode.Dev);
 
             string keyFile = null;
             string titleKeyFile = null;
@@ -236,7 +237,7 @@ namespace Ryujinx.HLE.FileSystem
 
             void LoadSetAtPath(string basePath)
             {
-                string localKeyFile = Path.Combine(basePath, "prod.keys");
+                string localKeyFile = Path.Combine(basePath, "dev.keys");
                 string localTitleKeyFile = Path.Combine(basePath, "title.keys");
                 string localConsoleKeyFile = Path.Combine(basePath, "console.keys");
 
@@ -284,6 +285,36 @@ namespace Ryujinx.HLE.FileSystem
                     if (titleKey != null)
                     {
                         KeySet.ExternalKeySet.Add(new RightsId(ticket.RightsId), new AccessKey(titleKey));
+                    }
+                }
+            }
+
+            foreach (DirectoryEntryEx ticketEntry in fs.EnumerateEntries("/", "*.tikenc"))
+            {
+                using var ticketFile = new UniqueRef<IFile>();
+
+                Result result = fs.OpenFile(ref ticketFile.Ref, ticketEntry.FullPath.ToU8Span(), OpenMode.Read);
+
+                if (result.IsSuccess())
+                {
+                    var tikEncStream = ticketFile.Get.AsStream();
+                    var tikEncBytes = new byte[tikEncStream.Length];
+                    tikEncStream.Read(tikEncBytes);
+
+                    var ticketBytes = new byte[tikEncBytes.Length - 0x80];
+
+                    if (HOS.Services.Fs.FileSystemProxy.FileSystemProxyHelper.DecryptTicket(ticketBytes, tikEncBytes))
+                    {
+                        using (var ms = new MemoryStream(ticketBytes, 0x20, ticketBytes.Length - 0x20))
+                        {
+                            Ticket ticket = new(ms);
+                            var titleKey = ticket.GetTitleKey(KeySet);
+
+                            if (titleKey != null)
+                            {
+                                KeySet.ExternalKeySet.Add(new RightsId(ticket.RightsId), new AccessKey(titleKey));
+                            }
+                        }
                     }
                 }
             }

asfasagag/RyuGreem-DevModePatch

src/Ryujinx.HLE/FileSystem/VirtualFileSystem.cs
@@ -27,9 +27,9 @@ namespace Ryujinx.HLE.FileSystem
 {
     public class VirtualFileSystem : IDisposable
     {
-        public static readonly string SafeNandPath = Path.Combine(AppDataManager.DefaultNandDir, "safe");
-        public static readonly string SystemNandPath = Path.Combine(AppDataManager.DefaultNandDir, "system");
-        public static readonly string UserNandPath = Path.Combine(AppDataManager.DefaultNandDir, "user");
+        public static readonly string SafeNandPath = Path.Combine(AppDataManager.DefaultNandDir, "safe_dev");
+        public static readonly string SystemNandPath = Path.Combine(AppDataManager.DefaultNandDir, "system_dev");
+        public static readonly string UserNandPath = Path.Combine(AppDataManager.DefaultNandDir, "user_dev");
 
         public KeySet KeySet { get; private set; }
         public EmulatedGameCard GameCard { get; private set; }
@@ -222,6 +222,7 @@ namespace Ryujinx.HLE.FileSystem
         public void ReloadKeySet()
         {
             KeySet ??= KeySet.CreateDefaultKeySet();
+            KeySet.SetMode(KeySet.Mode.Dev);
 
             string prodKeyFile = null;
             string titleKeyFile = null;
@@ -237,7 +238,7 @@ namespace Ryujinx.HLE.FileSystem
 
             void LoadSetAtPath(string basePath)
             {
-                string localProdKeyFile = Path.Combine(basePath, "prod.keys");
+                string localProdKeyFile = Path.Combine(basePath, "dev.keys");
                 string localTitleKeyFile = Path.Combine(basePath, "title.keys");
                 string localConsoleKeyFile = Path.Combine(basePath, "console.keys");
                 string localDevKeyFile = Path.Combine(basePath, "dev.keys");
@@ -291,6 +292,36 @@ namespace Ryujinx.HLE.FileSystem
                     if (titleKey != null)
                     {
                         KeySet.ExternalKeySet.Add(new RightsId(ticket.RightsId), new AccessKey(titleKey));
+                    }
+                }
+            }
+
+            foreach (DirectoryEntryEx ticketEntry in fs.EnumerateEntries("/", "*.tikenc"))
+            {
+                using var ticketFile = new UniqueRef<IFile>();
+
+                Result result = fs.OpenFile(ref ticketFile.Ref, ticketEntry.FullPath.ToU8Span(), OpenMode.Read);
+
+                if (result.IsSuccess())
+                {
+                    var tikEncStream = ticketFile.Get.AsStream();
+                    var tikEncBytes = new byte[tikEncStream.Length];
+                    tikEncStream.Read(tikEncBytes);
+
+                    var ticketBytes = new byte[tikEncBytes.Length - 0x80];
+
+                    if (HOS.Services.Fs.FileSystemProxy.FileSystemProxyHelper.DecryptTicket(ticketBytes, tikEncBytes))
+                    {
+                        using (var ms = new MemoryStream(ticketBytes, 0x20, ticketBytes.Length - 0x20))
+                        {
+                            Ticket ticket = new(ms);
+                            var titleKey = ticket.GetTitleKey(KeySet);
+
+                            if (titleKey != null)
+                            {
+                                KeySet.ExternalKeySet.Add(new RightsId(ticket.RightsId), new AccessKey(titleKey));
+                            }
+                        }
                     }
                 }
             }